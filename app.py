c5a7b03b24d70e81eaee4b12b196843d56bbc9af
roannapsr <120425299+roannapsr@users.noreply.github.com>
2026-09-17 18:08:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ st.set_page_config(
     page_title="Dra. Aninha - Perícia Previdenciária",
     page_icon=AVATAR_ANINHA,
     layout="wide"
+    initial_sidebar_state="expanded"
 )
 
 # Cliente Gemini
